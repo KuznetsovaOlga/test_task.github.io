065572e9500b22a1dd4ebbfad6dba1d76ab46350
Valery <krikotnenko@yahoo.com>
2021-04-06 19:50:10 +0300

    Updates

---
+++ precache-manifest.c0a34838238c41cfab3f3d08fce4bbdd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9cca02db9ee80c647c530f220ee9f70",
+    "url": "/test_task.github.io/index.html"
+  },
+  {
+    "revision": "fbb888ceb2347b1206a7",
+    "url": "/test_task.github.io/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "20f450fba455d7eff235",
+    "url": "/test_task.github.io/static/js/2.f73e330d.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/test_task.github.io/static/js/2.f73e330d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbb888ceb2347b1206a7",
+    "url": "/test_task.github.io/static/js/main.4323a524.chunk.js"
+  },
+  {
+    "revision": "af1d7e62ec7d58e50226",
+    "url": "/test_task.github.io/static/js/runtime-main.a46300ba.js"
+  },
+  {
+    "revision": "5199c3b016f3a8ca0931a26f6558cdc6",
+    "url": "/test_task.github.io/static/media/back.5199c3b0.png"
+  }
+]);
